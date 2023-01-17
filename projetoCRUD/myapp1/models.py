1fa4a154b346243b5f4c1d035ed8f5236a3b0a00
Paulo Archilha <pauloarchilha@gmail.com>
2023-01-17 11:59:07 -0300

    create CRUD

--- projetoCRUD/myapp1/models.py
+++ projetoCRUD/myapp1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    nome = models.CharField('nome', max_length=50)
+    telefone = models.BigIntegerField('telefone')
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'Nome: {self.nome}, Telefone: {self.telefone}, Email: {self.email}'
--- projetoCRUD/myapp1/views.py
+++ projetoCRUD/myapp1/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
 
 # Create your views here.
+
+
+def index(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+
+    return render(request, 'index.html', context)
+
+
+def create(request):
+
+    if request.method == 'GET':
+        form = UserForm()
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'criar.html', context=context)
+    else:
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+
+
+def modify(request, user_id):
+    context = {
+        'id': user_id,
+
+    }
+    return render(request, 'user/index.html', context=context)
